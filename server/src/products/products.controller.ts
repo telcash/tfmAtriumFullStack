b48ce73e479b9839caaa334dc31508a94cf3c5d9
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-07-10 16:56:02 +0200

    Guards agregados a endpoints de ProductsController
    Agregados Guards JwtAccessGuard y Role Guard a endpoints:
    create, update y remove

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(JwtAccessGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
@@ -22,11 +28,15 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
+  @UseGuards(JwtAccessGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
+  @UseGuards(JwtAccessGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
