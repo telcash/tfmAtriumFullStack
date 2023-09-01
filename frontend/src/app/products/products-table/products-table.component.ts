570b8318a0574953dd8d2e9bc0fc6d0407bcc886
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-09-01 16:25:38 +0200

    Ajustes en componentes

--- frontend/src/app/products/products-table/products-table.component.ts
+++ frontend/src/app/products/products-table/products-table.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductsService } from '../products.service';
 import { MatDialog } from '@angular/material/dialog';
 import { GlobalConstants } from 'src/app/config/global-constants';
 import { ConfirmDialogComponent, confirmDialogOptions } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { MatTable } from '@angular/material/table';
 
 /**
  * Componente que gestiona la tabla de productos
@@ -21,6 +22,9 @@ export class ProductsTableComponent implements OnInit {
   // Listado de productos
   products!: Product[];
 
+  // Referencia a la tabla de la vista
+  @ViewChild('table') table!: MatTable<any>;
+
   imgUrl: string = GlobalConstants.API_STATIC_PRODUCTS_IMG;
 
   constructor(
@@ -32,26 +36,6 @@ export class ProductsTableComponent implements OnInit {
    * Inicialización del componente
    */
   ngOnInit() {
-    this.getProductsList();
-  }
-
-  /**
-   * Método que elimina un producto
-   * @param {number} productId - Id del producto
-   */
-  deleteProduct(productId: number) {
-    
-    // Llamada al servicio para solicitud al API de la eliminación del producto
-    this.productsService.deleteProduct(productId).subscribe(() => {
-      this.getProductsList();
-    });
-  }
-
-  /**
-   * Método que carga la lista de productos
-   */
-  getProductsList() {
-
     // LLamada al servicio para la solicitud al API del listado de productos
     this.productsService.getAllProducts().subscribe((
       (data)  => {
@@ -63,10 +47,29 @@ export class ProductsTableComponent implements OnInit {
   }
 
   /**
-   * Metodo que abre un dialogo de confirmación de eliminación de un producto
-   * @param {number} productId - Id del producto 
+   * Método que elimina un producto
+   * @param {number} productId - Id del producto
+   * @param {number} tableIndex - Índice en la tabla de la vista del producto a a eliminar
    */
-  openDeleteDialog(productId: number) {
+  deleteProduct(productId: number, tableIndex: number) {
+    
+    // Llamada al servicio para solicitud al API de la eliminación del producto
+    this.productsService.deleteProduct(productId).subscribe(() => {
+      
+      // Elimina el producto del listado de la tabla
+      this.products.splice(tableIndex, 1);
+
+      // Solicitud de renderizado a la tabla
+      this.table.renderRows();
+    });
+  }
+
+  /**
+   * Metodo que abre un dialogo de confirmación de eliminación de un producto
+   * @param {number} productId - Id del producto
+   * @param {number} tableIndex - Índice en la tabla de la vista del producto a a eliminar
+   */
+  openDeleteDialog(productId: number, tableIndex: number) {
 
     // Creación del componente que genera el diálogo
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
@@ -80,7 +83,7 @@ export class ProductsTableComponent implements OnInit {
     // Si el diálogo cierra con una confirmación invoca al método deleteProduct()
     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
       if (confirmed) {
-        this.deleteProduct(productId);
+        this.deleteProduct(productId, tableIndex);
       }
     })
   }
--- frontend/src/app/users/users-table/users-table.component.ts
+++ frontend/src/app/users/users-table/users-table.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/auth/models/user';
 import { UsersService } from '../users.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent, confirmDialogOptions } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-users-table',
@@ -17,6 +18,9 @@ export class UsersTableComponent implements OnInit {
   // Listado de usuarios
   users!: User[];
 
+  // Referencia a la tabla de la vista
+  @ViewChild('table') table!: MatTable<any>;
+
   constructor(
     private usersService: UsersService,
     private dialog: MatDialog,
@@ -26,16 +30,7 @@ export class UsersTableComponent implements OnInit {
    * Inicialización del componente
    */
   ngOnInit(): void {
-      this.getUsersList();
-  }
-
-
-  /**
-   * Método que obtiene el listado de los usuarios
-   */
-  getUsersList() {
-
-    // LLamada al servicio para solicitud al API del listado de los usuarios
+      // LLamada al servicio para solicitud al API del listado de los usuarios
     this.usersService.getAllUsers().subscribe(
       users => {
 
@@ -48,20 +43,27 @@ export class UsersTableComponent implements OnInit {
   /**
    * Método que elimina un usuario
    * @param {number} userId - Id del usuario 
+   * @param {number} tableIndex - Índice en la tabla de la vista del usuario a eliminar 
    */
-  deleteUser(userId: number) {
+  deleteUser(userId: number, tableIndex: number) {
 
     // LLamada al servicio para solicitud al API de eliminación de un usuario
     this.usersService.deleteUser(userId).subscribe(() => {
-      this.getUsersList();
+      
+      // Elimina al usuario del listado de la tabla
+      this.users.splice(tableIndex, 1);
+
+      // Solicitud de renderizado de la tabla
+      this.table.renderRows();
     })
   }
 
   /**
    * Método que abre un diálogo de confirmación de eliminación de un usuario
+   * @param {number} tableIndex - Índice en la tabla de la vista del usuario a eliminar 
    * @param {number} userId - Id del usuario
    */
-  openDeleteDialog(userId: number) {
+  openDeleteDialog(userId: number, tableIndex: number) {
 
     // Crea el componente que general el diálogo
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
@@ -75,7 +77,7 @@ export class UsersTableComponent implements OnInit {
     // Si el diálogo cierra con una confirmación de eliminación, invoca al método deleteUser()
     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
       if(confirmed) {
-        this.deleteUser(userId);
+        this.deleteUser(userId, tableIndex);
       }
     })
   }
