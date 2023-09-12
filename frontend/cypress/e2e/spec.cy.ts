1be40d75ae8b60b9e63c4a5bf9feeb266e3e3d66
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-09-12 08:05:04 +0200

    Ajustes finales

--- frontend/cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-  beforeEach('Checks home page title', () => {
-    cy.visit('/');
-  });
-  it('has the correct title', () => {
-    cy.title().should('equal', 'charliesabores');
-  });
-});
