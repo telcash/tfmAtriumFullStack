91ea32ff71b8e8a3ea40e8921f942195bc8feae2
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2024-01-20 23:49:18 +0100

    Cambio en url server

--- frontend/src/app/config/global-constants.ts
+++ frontend/src/app/config/global-constants.ts
@@ -3,6 +3,6 @@
  */
 
 export class GlobalConstants {
-    static readonly API_URL = 'http://localhost:3000';
-    static readonly API_STATIC_PRODUCTS_IMG = 'http://localhost:3000/img/products';
+    static readonly API_URL = 'http://34.224.98.21:3000';
+    static readonly API_STATIC_PRODUCTS_IMG = 'http://34.224.98.21:3000/img/products';
 }
